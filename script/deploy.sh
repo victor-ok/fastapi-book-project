a0aaecbf24f84a548d236f7fcd9b7c1193234e01
victor-ok <vua.okafor@gmail.com>
2025-02-11 10:37:55 +0100

    push changes

--- script/deploy.sh
+++ script/deploy.sh
@@ -21,27 +21,27 @@ if ! command -v nginx > /dev/null; then
     sudo apt install -y nginx
 fi
 
-# if [ ! -f /etc/nginx/sites-available/app ]; then
-#     sudo rm -f /etc/nginx/sites-enabled/default
-#     sudo bash -c 'cat > /etc/nginx/sites-available/app <<EOF
-#     server {
-#     listen 80;
-#     server_name localhost;
+if [ ! -f /etc/nginx/sites-available/app ]; then
+    sudo rm -f /etc/nginx/sites-enabled/default
+    sudo bash -c 'cat > /etc/nginx/sites-available/app <<EOF
+    server {
+    listen 80;
+    server_name localhost;
 
-#     location / {
-#         proxy_pass http://api:8000;
-#         proxy_set_header Host $host;
-#         proxy_set_header X-Real-IP $remote_addr;
-#         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-#         proxy_set_header X-Forwarded-Proto $scheme;
-#     }
-# }
-# EOF'
-#     sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
-#     sudo systemctl restart nginx
-# else
-#     echo "Nginx reverse proxy configuration already configured"
-# fi
+    location / {
+        proxy_pass http://127.0.0.1:8000;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+}
+EOF'
+    sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
+    sudo systemctl restart nginx
+else
+    echo "Nginx reverse proxy configuration already configured"
+fi
 
 echo "pull main branch"
 git pull origin main
--- tests/test_books.py
+++ tests/test_books.py
@@ -14,6 +14,8 @@ def test_get_single_book():
     assert data["title"] == "The Hobbit"
     assert data["author"] == "J.R.R. Tolkien"
 
+    response = client.get("/books/70")
+    assert response.status_code == 404
 
 def test_create_book():
     new_book = {
